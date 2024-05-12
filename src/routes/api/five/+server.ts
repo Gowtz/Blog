dfb04bcc4cf045847b1be2773e500e6b906441ba
Gowtham <gajavijay8@gmai.com>
2024-05-13 01:37:10 +0530

    checking out branch

--- src/routes/api/five/+server.ts
+++ src/routes/api/five/+server.ts
@@ -1,7 +1,6 @@
 import db from "$lib/firebase";
 import type { RequestHandler } from "@sveltejs/kit";
 import { collection, getDocs, limit, query } from "firebase/firestore/lite";
-
 export const GET: RequestHandler = async () => {
     const post = collection(db,'posts');
     const q= query(post,limit(4));
